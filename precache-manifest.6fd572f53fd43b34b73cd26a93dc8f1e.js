a4d723e7aea2cc175fbc7011a77845677dd7f686
axeemhaider <axeemhaider@gmail.com>
2020-07-23 15:10:35 +0500

    Updates

---
+++ precache-manifest.6fd572f53fd43b34b73cd26a93dc8f1e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08955da9313914b1d434cf6f8865df2b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6711dd0a359e50b9ab82",
+    "url": "/static/css/2.9d5983be.chunk.css"
+  },
+  {
+    "revision": "204009f581620d926091",
+    "url": "/static/css/main.ed1f2490.chunk.css"
+  },
+  {
+    "revision": "6711dd0a359e50b9ab82",
+    "url": "/static/js/2.5f5f1f80.chunk.js"
+  },
+  {
+    "revision": "c527f6856b108f91426fceea77d6000d",
+    "url": "/static/js/2.5f5f1f80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "204009f581620d926091",
+    "url": "/static/js/main.33a963f6.chunk.js"
+  },
+  {
+    "revision": "b8bb20ad1dd609fde98e",
+    "url": "/static/js/runtime-main.e3d10730.js"
+  }
+]);
